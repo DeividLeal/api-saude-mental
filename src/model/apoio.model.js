4303b456888fcdba3ca30d7339b090b2cf04dcdf
Deivid Leal <deivid.caldass@gmail.com>
2025-07-09 12:39:07 -0300

    new apoio

--- src/model/apoio.model.js
+++ src/model/apoio.model.js
@@ -8,8 +8,24 @@ class CanalDeApoio {
 }
 
 export const canaisDeApoio = [
-  new CanalDeApoio("CVV - Centro de Valorização da Vida", "188", "https://www.cvv.org.br/", "Apoio emocional e prevenção do suicídio."),
-  new CanalDeApoio("SUS - CAPS", "Atendimento psicológico gratuito pelo Sistema Único de Saúde (CAPS)", null, "Serviço público de saúde mental.")
+  new CanalDeApoio(
+    "CVV - Centro de Valorização da Vida",
+    "188",
+    "https://www.cvv.org.br/",
+    "Apoio emocional e prevenção do suicídio."
+  ),
+  new CanalDeApoio(
+    "SUS - CAPS",
+    "Atendimento psicológico gratuito pelo Sistema Único de Saúde (CAPS)",
+    null,
+    "Serviço público de saúde mental."
+  ),
+  new CanalDeApoio(
+    "Vidas Preservadas",
+    "0800 280 2828",
+    "https://www.vidaspreservadas.com.br/",
+    "Programa de apoio e prevenção ao suicídio."
+  )
 ];
 
 export function listarCanaisApoio() {
